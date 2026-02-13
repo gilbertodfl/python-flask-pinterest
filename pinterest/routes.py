3cd2c2ad4ee58e75368fc0b5b0e44893d38075ff
gilbertodfl <gilbertofl@tjdft.jus.br>
2026-02-12 21:20:49 -0300

    login atualizado

--- pinterest/routes.py
+++ pinterest/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, flash
 from flask_login import login_required, login_user, logout_user, current_user
 from sqlalchemy.sql.functions import user
 from wtforms import Form
@@ -7,15 +7,16 @@ from pinterest.models import Usuario, Foto
 from pinterest.forms import FormCriarConta, FormLogin
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 @app.route('/home', methods=['GET', 'POST'])
 def homepage():
     form = FormLogin()
     if form.validate_on_submit():
         usuario = Usuario.query.filter_by(email=form.email.data).first()
-        if usuario and  bcrypt.check_password_hash(usuario.password, form.password.data):
-                login_user(usuario, remember=form.remember_me.data)
-                return redirect(url_for('perfil', usuario=usuario.username))
+        if usuario and bcrypt.check_password_hash(usuario.password, form.password.data):
+            login_user(usuario, remember=form.remember_me.data)
+            return redirect(url_for('perfil', id_usuario=usuario.id))
+        flash('Email ou senha incorretos.', 'alert-danger')
     return render_template('homepage.html', form=form)
 
 @app.route('/criarconta', methods=['GET', 'POST'])
@@ -25,7 +26,7 @@ def criarconta():
         usuario = Usuario()
         usuario.email = form.email.data
         usuario.username = form.username.data
-        usuario.password = bcrypt.generate_password_hash(form.username.data)
+        usuario.password = bcrypt.generate_password_hash(form.password.data)
 
         db.session.add(usuario)
         db.session.commit()
@@ -38,13 +39,21 @@ def criarconta():
         ## força o login
         login_user(usuario, remember=True)
         # Redirecionar para a página de login ou perfil após criar a conta
-        return redirect( url_for('perfil', usuario=usuario.username))  # Redireciona para a homepage ou outra página após criar a conta
+        return redirect( url_for('perfil', id_usuario=usuario.id))  # Redireciona para a homepage ou outra página após criar a conta
     return render_template('criarconta.html', form=form)
 
 
 @login_required
-@app.route('/perfil/<usuario>')
-def perfil(usuario):
+@app.route('/perfil/<id_usuario>')
+def perfil(id_usuario):
+    if int(id_usuario) == current_user.id:
+
+        ## usuario acesso seu perfil
+                return render_template('perfil.html', usuario=current_user)
+    else:
+        user = Usuario.query.get(id_usuario)
+        return render_template('perfil.html', usuario=user)
+
     return render_template('perfil.html', usuario=usuario)
 
 @app.route('/logout')
